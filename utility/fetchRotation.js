ed56fe111b51916a9c2bf87803007df1a748a377
Thierry Kühni <thierry.kuehni@students.fhnw.ch>
2021-01-29 20:25:44 +0100

    changed data from ini to json

--- utility/fetchRotation.js
+++ utility/fetchRotation.js
@@ -1,28 +1,26 @@
 const fs = require("fs");
 const config = require("config");
 const fetch = require("node-fetch");
-const ini = require("js-ini");
 const streamToString = require("stream-to-string");
 const  {DateTime} = require("luxon");
 
-const {setRotationDate, getRotationDate, setRotations} = require("../rotation");
+const {setRotationDate, getRotationDate, setRotations, setOffset} = require("../rotation");
 
-const rotationFilePath = "./data/rotation.ini";
-const brackets = Object.keys(config.get("options.rotationFile"));
+const rotationFilePath = "./data/rotation.json";
 
 async function downloadRotation(){
     const res = await fetch(config.get("options.rotationURL"), {
         method: 'GET',
-        headers: {"Content-Type": "text/html; charset=utf-8"},
+        headers: {"Content-Type": "application/json"},
         body: null
     });
 
     const string = await streamToString(res.body);
 
-    const rotationsDateLine = string.split("\n")[0];
-    const rotationsDate = rotationsDateLine.substring(1).replace("\r", "");
+    const {data} = JSON.parse(string);
 
-    const date = DateTime.fromFormat(rotationsDate, "yyyy-MM-dd hh:mm");
+    const date = DateTime.fromFormat(data.date, "yyyy-MM-dd hh:mm");
+    
 
     //Data up to date
     if(getRotationDate().equals(date)){
@@ -32,7 +30,7 @@ async function downloadRotation(){
     setRotationDate(date);
 
     //Save rotation to file
-    fs.writeFile(rotationFilePath, string.replace(rotationsDateLine, ""), (err)=>{
+    fs.writeFile(rotationFilePath, JSON.stringify(data), (err)=>{
         if(err){
             throw new Error("Failed to save rotation file: " + err);
         }
@@ -47,18 +45,11 @@ function updateRotation(){
             throw new Error("Failed to read rotation file: " + err);
         }
 
-        const rotation = ini.parse(data);
+        const rotation = JSON.parse(data);
       
-        var rotations = [[], [], [], [], [], []];
+        setRotations(rotation.missions);
 
-        for(var k = 0; k < 6; k++){
-            for(var i = 1 ; i < 11 ; i++){
-                rotations[k].push(rotation[config.get("options.rotationFile."+brackets[k])][i]);
-            }
-        }
-
-        setRotations(rotations);
-
+        setOffset(rotation.minutesOffset, rotation.secondsOffset);
     });
 }
 
